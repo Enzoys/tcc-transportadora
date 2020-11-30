f7d40c2f60b97e383252e5c73da70454c0025056
eugeniofear <eugeniofear@hotmail.com>
2020-11-30 18:05:24 -0300

    adição no index

--- index.php
+++ index.php
@@ -45,6 +45,19 @@
         </div>
     </div>
 </div>
+
+<div style="background-color: red;" >
+    <div style="height: 100%; min-height: 300px; width: 100%; margin: 0 25%;">
+        <div style="background-color: purple; height: 100%; min-height: 300px; width: 25%; margin: 0;float:left;">
+
+        </div>        
+    
+        <div style="background-color: orange; height: 100%; width: 50%; min-height: 300px; margin: 0;">
+            <h2>Contate-nos!</h2>
+        </div>
+    </div>
+</div>
+
 <div class="branca" style="background-color: black; height: 100%; min-height: 450px; width: 100%;">
     <div style="text-align: center; padding-top: 70px;">
         <h1>NOSSA HISTÓRIA</h1>
@@ -58,6 +71,7 @@
     </div>
 </div>
 <div style="text-align: center; padding-top: 0px;">
+
     <div style="padding-top: 60px; width: 50%; margin: 0 25%;">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3677.197240865866!2d-43.27703718441968!3d-22.83219104095501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997bbeedb6abbb%3A0x81d7347085e9cebe!2sR.%20Beliz%C3%A1rio%20Pena%2C%20450%20-%20Penha%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021020-010!5e0!3m2!1spt-BR!2sbr!4v1606514206032!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
