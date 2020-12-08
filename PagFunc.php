097f07a412a643730f5295403bc08436743f443b
Enzo <ilhadosmeros10@gmail.com>
2020-12-08 12:41:15 -0300

    Mudança pagFunc, css body

--- PagFunc.php
+++ PagFunc.php
@@ -3,12 +3,18 @@
     include "cabecalho.php";
 ?>
 <title>Página Funcionário</title>
-<center>
-    <a href="PagFuncCliPessoas.php">CLIENTE</a><br />
-    <a href="PagFuncCliEmpresas.php">EMPRESA</a>
-</center>
-<br />
-<a href="sairSessao.php">Sair</a>
+<div style="background-color: lightyellow; width:70%; height: 100%; float:right">
+    
+</div>
+<div style="background-color: lightcoral; width:30%; height: 100%">
+    <center>
+        <a href="PagFuncCliPessoas.php">CLIENTE</a><br />
+        <a href="PagFuncCliEmpresas.php">EMPRESA</a>
+    </center>
+    <br />
+    <a href="sairSessao.php">Sair</a>
+</div>
+
 <?php
     include "rodape.php";
 ?>
