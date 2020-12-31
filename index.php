ade673c8815efa859866c1fe95f50ece27b888ed
eugeniofear <eugeniofear@hotmail.com>
2020-12-30 21:55:20 -0300

    slide/css/js/bootstrap/correção de bugs/etc...

--- index.php
+++ index.php
@@ -24,16 +24,44 @@
 </div>
 <!-- Photo grid (modal) -->
 <div class="w3-row-padding">
-   <div class="w3-half w3-left">
-      <img src="imagens/caminhao4.jpg" style="width:100%" onclick="onClick(this)" alt="cm1">
-      <img src="imagens/caminhao3.jpg" style="width:100%" onclick="onClick(this)" alt="cm2">
-      <img src="imagens/caminhao1.png" style="width:100%" onclick="onClick(this)" alt="cm3">
-   </div>
-   <div class="w3-half w3-right">
-      <img src="imagens/caminhao4.jpg" style="width:100%" onclick="onClick(this)" alt="cm4">
-      <img src="imagens/caminhao3.jpg" style="width:100%" onclick="onClick(this)" alt="cm5">
-      <img src="imagens/caminhao1.png" style="width:100%" onclick="onClick(this)" alt="cm6">
-   </div>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagens/sede.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/sede.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="imagens/sede.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </div>
 <!-- Modal for full size images on click-->
 <div id="modal01" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
@@ -96,32 +124,32 @@
    </div>
 </div>
 <!-- Packages / Pricing Tables -->
-<div class="w3-container" id="packages" style="margin-top:75px">
-   <h1 class="w3-xxxlarge w3-text-red"><b>Nossa localização</b></h1>
+<div class="w3-container" style="margin-top:75px">
+   <h1 class="w3-xxxlarge w3-text-red"><b>Nossa localização.</b></h1>
    <hr style="width:27%;border:5px solid red" class="w3-round">
 </div>
-<div class="w3-half">
-   <div class="w3-center">
-      <ul class="w3-ul w3-light-grey w3-right">
-         <li class="w3-red w3-xlarge w3-padding-16">Mapa</li>
-         <div style="width: 100%; margin: 0%;">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3677.197240865866!2d-43.27703718441968!3d-22.83219104095501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997bbeedb6abbb%3A0x81d7347085e9cebe!2sR.%20Beliz%C3%A1rio%20Pena%2C%20450%20-%20Penha%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021020-010!5e0!3m2!1spt-BR!2sbr!4v1606514206032!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-         </div>
-         </li>
-      </ul>
-   </div>
-</div>
-<div class="w3-half">
-   <div class="w3-center">
-      <ul class="w3-ul w3-light-grey w3-right">
-         <li class="w3-red w3-xlarge w3-padding-16">Sede</li>
-         <div style="width: 100%; margin: 0%;">
-            <img src="imagens/sede.png" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-         </div>
-         </li>
-      </ul>
-   </div>
-</div>
+    <div class="container">
+      <div class="row">
+        <div class="card col-sm">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3677.197240865866!2d-43.27703718441968!3d-22.83219104095501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997bbeedb6abbb%3A0x81d7347085e9cebe!2sR.%20Beliz%C3%A1rio%20Pena%2C%20450%20-%20Penha%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2021020-010!5e0!3m2!1spt-BR!2sbr!4v1606514206032!5m2!1spt-BR!2sbr" width="540" height="355" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+        <div class="card col-sm">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+          <img src="imagens/sede.png" class="card-img-bottom" alt="...">
+        </div>
+      </div>
+    </div>
+  </div>
 <!-- Contato -->
 <div class="w3-container" id="contato" style="margin-top:75px">
    <h1 class="w3-xxxlarge w3-text-red"><b>Contato.</b></h1>
@@ -155,5 +183,8 @@
    captionText.innerHTML = element.alt;
    }
 </script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
