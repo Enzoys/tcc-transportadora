f35de976ec2cb5b222c3beeb9aa8d41051266622
Joauww <joaopedro.tecinf20@gmail.com>
2021-01-27 21:21:33 -0300

    teste padding

--- PagFuncCliPessoas.php
+++ PagFuncCliPessoas.php
@@ -4,7 +4,7 @@
 ?>
 <title>Funcionário Página Cliente Pessoa Física</title>
 <div style="background-color: #343a40; width:100%; height: 100%; float:right">
-    <div style="text-align: center;padding: 0px">
+    <div style="text-align: center;padding: 40%">
         <h2 style="color:white;">Gerenciar Clientes - Pessoas Físicas</h2>        
         <br/><a class="btn btn-primary btn-lg" href="PagCliPessoaCadastro.php">CADASTRAR PESSOA FÍSICA</a><br>
         <br><a class="btn btn-secondary btn-lg" href="PagFuncPesquisaCliPessoa.php">PESQUISAR PESSOA FÍSICA</a><br>
