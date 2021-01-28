4dd13119f8a4b4950990568767d29eca0c7bd346
eugeniofear <eugeniofear@hotmail.com>
2021-01-27 21:41:00 -0300

    bug

--- PagAdmFuncionarios.php
+++ PagAdmFuncionarios.php
@@ -12,20 +12,6 @@
         <br><a class="btn btn-secondary btn-lg" href="PagAdmPesquisaFuncionario.php">PESQUISAR FUNCIONÁRIO</a><br />
     </div>
 </div>
-<div style="background-color: #ea8a82; width:30%; height: 100%; text-align: center;">
-        TRANSPORTES<br />
-        <a href="PagFuncPedidos.php">Pedidos</a><br />
-        <a href="PagFuncCriarViagem.php">Novo transporte</a><br />
-        <a href="PagFuncCadastroMotorista.php">Cadastrar Motorista</a><br />
-        <br />CLIENTES<br />
-        <a href="PagFuncCliPessoas.php">Pessoas Físicas</a><br />
-        <a href="PagFuncCliEmpresas.php">Empresas</a><br />
-        <br />ADMINISTRADOR<br />
-        <a href="PagAdmFuncionarios.php">Funcionários</a><br>
-    <br />
-    <a href="sairSessao.php">Sair</a><br/>
-    <br /><a href='PagAdm.php'>VOLTAR</a>
-</div>
 <?php
     include "rodape.php";
 ?>
